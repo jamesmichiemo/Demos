b9b7a96d3382e0af18a2ce38ef0a603eb08f8139
Jean Amarante <jeanamarantec@gmail.com>
2013-10-17 03:04:01 -0400

    Was bored :)

--- Percolation/js/main.js
+++ Percolation/js/main.js
@@ -6,6 +6,9 @@
     
     'use strict';
     
+    /*
+        Basic colors for percolation grid.
+    */
     var Color = {
             black: '#000000',
             blue: '#00B7FF',
@@ -21,16 +24,35 @@
         this.width = width;
         this.height = height;
     }
-    Tile.prototype = {
-        isFilled: false,    // Is it filled with water?
-        isEmpty: false,     // Is it still blocked?
-        color: Color.black  // black = blocked, blue = filled, white = empty
-    };
+    Tile.prototype = (function () {
+        var isFilled = false,       // Is it filled with water?
+            isEmpty = false,        // Is it empty?
+            color = Color.black,    // black = blocked, blue = filled, white = empty
+            state = 'blocked';      // Keep track of the current state.
+            
+        return {
+            fillTile: function () {
+                isFilled = true;
+                isEmpty = false;
+                color = Color.blue;
+                state = 'filled';
+            },
+            pullTile: function () {
+                isEmpty = true;
+                color = Color.white;
+                state = 'empty';
+            },
+            getState: function () {
+                return state;
+            }
+        };
+    }());
     
     /*
         The Grid serves as the container for all the tiles.
     */
     function Grid(rows, cols) {
+        
         if (typeof rows === Number || rows <= 0) {
             return;
         } else if (typeof cols === Number || cols <= 0) {
@@ -40,26 +62,27 @@
         this.rows = rows;
         this.cols = cols;
         this.tileCount = rows * cols;
+        this.tilesRemoved = 0;
+        this.tiles = [];
         
-        var r, c, x, y, w, h;
+        var r,
+            c,
+            x = 0,
+            y = 0,
+            w,
+            h;
         
         for (r = 0; r < rows; r += 1) {
             this.tiles[r] = [];
             for (c = 0; c < cols; c += 1) {
-                x = 0;
-                y = 0;
+//                x = 0;
+//                y = 0;
                 this.tiles[r][c] = new Tile(x, y, w, h);
             }
         }
+        
+        
     }
-    Grid.prototype = {
-        rows: 0,
-        cols: 0,
-        tiles: [],
-        tileCount: 0,
-        tilesRemoved: 0,
-        percolated: false
-    };
     
     /*
         Use the canvas element to display the Percolation.
